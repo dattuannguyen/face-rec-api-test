a93809c1d80ab0081a36c1d949da8f01e09d2844
dattuannguyen <dat.nt0609@gmail.com>
2023-11-21 09:42:10 +0100

    change the port

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.put('/image',(req,res)=>{
 app.post('/imageurl',(req,res)=>{
     image.handleApiCall(req,res)
 })
-app.listen(3000,()=>{
-    console.log('app is running in port 3000');
+app.listen(5432,()=>{
+    console.log('app is running in port 5432');
 })
